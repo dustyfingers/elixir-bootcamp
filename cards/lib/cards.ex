69c2611bf70d647885d75ec1e499284b9c68def1
lou <dustyfingers01257@gmail.com>
2021-12-04 21:42:47 -0700

    learned about method arguments and the standard library in elixir

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,8 +1,9 @@
 # defmodule creates a 
 # module is a collection of methods or functions
 defmodule Cards do
+
   # def defines a method in this module
-  def hello do
+  def hello_world do
     # implicit return last value in function
     "hi there!"
   end
@@ -11,4 +12,12 @@ defmodule Cards do
     # brackets define a list
     ["Ace", "Two", "Three"]
   end
+
+  # shuffle method takes a 'deck' as an argument
+  # elixir forces you to call methods with the correct number of arguments
+  def shuffle(deck) do 
+    # Enum is part of the elixir standard library
+    Enum.shuffle(deck)
+  end
+
 end

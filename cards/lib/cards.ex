57356c7c6d222c9082e23b75737127012d9f7a8c
lou <dustyfingers01257@gmail.com>
2021-12-05 08:05:57 -0700

    built contains method and finished create_deck

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -10,14 +10,44 @@ defmodule Cards do
 
   def create_deck do
     # brackets define a list
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # list comprehensions
+    # a comprehension is a mapping function
+
+    # BAD APPROACH IN ELIXIR
+    # for suit <- suits do
+
+    #   for value <- values do
+
+    #     "#{value} of #{suit}"
+
+    #   end
+
+    # end
+
+    # better way to do multiple comprehensions in elixir
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
   end
 
   # shuffle method takes a 'deck' as an argument
   # elixir forces you to call methods with the correct number of arguments
   def shuffle(deck) do 
     # Enum is part of the elixir standard library
+    # this does NOT mutate the 'deck' variable - it returns a new list
     Enum.shuffle(deck)
+  end
+
+  # ? is valid in method names in elixir
+  # doesnt impart any special funcitonality
+  # mainly used to denote that the method returns 'truthy' or 'falsey'
+  # rule of thumb - used when 'asking a question' about a certain data structure
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 
 end

70520e5f681826e44197fa553ef25da8557e1988
lou <dustyfingers01257@gmail.com>
2021-12-11 16:22:18 -0700

    wrote another test

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -10,6 +10,17 @@ defmodule CardsTest do
   # test "the truth" do
   #   assert 1 + 1 == 2
   # end
-  
+
+  test "create_deck makes 52 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    assert deck != Cards.shuffle(deck)
+    # you could also do 'refute deck == Cards.shuffle(deck)'
+  end
+
 end
     
